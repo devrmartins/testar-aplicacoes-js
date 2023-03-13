33fac7af7e47003d42684338eb342a645beeb931
Raffael Martins <devrmartins@gmail.com>
2023-03-13 13:43:15 -0300

    Aula 03

--- projeto01/src/lib/calculator.js
+++ projeto01/src/lib/calculator.js
@@ -1,3 +1,10 @@
 module.exports.sum = (num1, num2) => {
-    return num1 + num2;
+    const n1 = parseInt(num1, 10);
+    const n2 = parseInt(num2, 10);
+
+    if (Number.isNaN(n1) || Number.isNaN(n2)) {
+        throw new Error("Please check your input");
+    }
+
+    return parseInt(num1,10) + parseInt(num2,10);
 }
--- projeto01/src/lib/calculator.spec.js
+++ projeto01/src/lib/calculator.spec.js
@@ -3,3 +3,13 @@ const { sum } = require("./calculator");
 it("should sum 2 and 2 and the result must be 4", () => {
     return expect(sum(2,2)).toBe(4);
 })
+
+it("should sum 2 and 2 even if one of them is a string the result must be 4", () => {
+    return expect(sum("2",2)).toBe(4);
+})
+
+it("should throw an error if what is provided to the method not be a number", () => {
+    return expect(() => {
+        sum('',2);
+    }).toThrowError()
+})
