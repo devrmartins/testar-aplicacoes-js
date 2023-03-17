65087ade789cf6b3599e6a97ce8462fd5b9a1dea
Raffael Martins <devrmartins@gmail.com>
2023-03-16 22:45:33 -0300

    Aula 10

--- projeto01/src/lib/queryString.js
+++ projeto01/src/lib/queryString.js
@@ -1,5 +1,21 @@
-module.exports = (obj) => {
-    return Object.entries(obj)
-        .map(item => `${item[0]}=${item[1]}`)
-        .join('&');
-}
+const keyAndValueToString = ([key, value]) => {
+    if (typeof value === "object" && !Array.isArray(value)) {
+        throw new Error("Please check your params");
+    }
+    return `${key}=${value}`;
+};
+
+module.exports.queryString = (obj) =>
+    Object.entries(obj).map(keyAndValueToString).join("&");
+
+module.exports.parse = (value) =>
+    Object.fromEntries(
+        value.split("&").map((item) => {
+            let [key, value] = item.split("=");
+            if (value.indexOf(",") > -1) {
+                value = value.split(",");
+            }
+
+            return [key, value];
+        })
+    );
--- projeto01/src/lib/queryString.spec.js
+++ projeto01/src/lib/queryString.spec.js
@@ -1,13 +1,66 @@
-const queryString = require("./queryString")
+const { queryString, parse } = require("./queryString");
 
 describe("Object to query string", () => {
     it("should create a valid queryStrig  when an Object is provided", () => {
         const obj = {
             name: "Raffael",
-            profession: "Developer"
-        }
+            profession: "Developer",
+        };
 
-        expect(queryString(obj))
-        .toBe(`name=Raffael&profession=Developer`)
-    })
-})
+        expect(queryString(obj)).toBe(`name=Raffael&profession=Developer`);
+    });
+
+    it("should create a valid queryString even when an array is passed", () => {
+        const obj = {
+            name: "Raffael",
+            abilities: ["JS", "TDD"],
+        };
+
+        expect(queryString(obj)).toBe(`name=Raffael&abilities=JS,TDD`);
+    });
+
+    it("should throw an error when an object is passed as value", () => {
+        const obj = {
+            name: "Raffael",
+            abilities: {
+                first: "JS",
+                second: "TDD",
+            },
+        };
+
+        expect(() => {
+            queryString(obj);
+        }).toThrowError();
+    });
+});
+
+describe("Query string to object", () => {
+    it("should convert a query string to object", () => {
+        const query = "name=Raffael&profession=Developer";
+        const expectedObj = {
+            name: "Raffael",
+            profession: "Developer",
+        };
+
+        expect(parse(query)).toEqual(expectedObj);
+    });
+
+    it("should convert a query string of a single key-value pair to object", () => {
+        const query = "name=Raffael";
+        const expectedObj = {
+            name: "Raffael",
+        };
+
+        expect(parse(query)).toEqual(expectedObj);
+    });
+
+    it("should convert a query string to an object taking care of comma separated values", () => {
+        const query = "name=Raffael&abilities=JS,TDD";
+        const expectedObj = {
+            name: "Raffael",
+            abilities: ["JS", "TDD"],
+        };
+
+        expect(parse(query)).toEqual(expectedObj);
+    });
+});
